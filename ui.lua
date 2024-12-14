f699a13be4ee0cc3bb412deedba5dee136b0c5db
No6no6no7 <142609720+No6No6No7yt@users.noreply.github.com>
2024-12-14 18:50:42 +0100

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -60,33 +60,7 @@ function Library:CreateWindow(hubname)
     end;
 
     -- Instances:
-    local ScreenGui = Instance.new("ScreenGui")
-    local Body = Instance.new("Frame")
-    local Body_Corner = Instance.new("UICorner")
-    local Title_Hub = Instance.new("TextLabel")
-    local MInimize_Button = Instance.new("ImageButton")
-    local Discord = Instance.new("TextButton")
-    local UICorner = Instance.new("UICorner")
-    local Disc_Logo = Instance.new("ImageLabel")
-    local Disc_Title = Instance.new("TextLabel")
-    local Server_Time = Instance.new("TextLabel")
-    local Server_ID = Instance.new("TextLabel")
-    local List_Tile = Instance.new("Frame")
-    local Tile_Gradient = Instance.new("UIGradient")
-    local Toggle = Instance.new("Frame")
-    local toggle_corner = Instance.new("UICorner")
-    local toggle_Image = Instance.new("ImageButton")
 
-    -- Properties:
-    ScreenGui.Name = LibName
-    ScreenGui.Parent = game.CoreGui
-    ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global
-
-    game:GetService("UserInputService").InputBegan:connect(function(input) 
-        if input.KeyCode == Enum.KeyCode.LeftControl then
-            Library:ToggleUI()
-        end
-    end)
 
     Body.Name = "Body"
     Body.Parent = ScreenGui
@@ -124,8 +98,12 @@ function Library:CreateWindow(hubname)
     MInimize_Button.Position = UDim2.new(0, 570, 0, 0)
     MInimize_Button.Rotation = -315
     MInimize_Button.Size = UDim2.new(0, 30, 0, 30)
-    MInimize_Button.Image = ""
-    MInimize_Button.ImageColor3 = Color3.fromRGB(255, 255, 255)
+    MInimize_Button.AutoButtonColor = false
+    MInimize_Button.Font = Enum.Font.SourceSans
+    MInimize_Button.Text = "+"
+    MInimize_Button.TextColor3 = Color3.fromRGB(255, 255, 255)
+    MInimize_Button.TextSize = 40.000
+    MInimize_Button.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
 
     Discord.Name = "Discord"
     Discord.Parent = Body
@@ -180,7 +158,7 @@ function Library:CreateWindow(hubname)
     end)
 
     Discord.MouseButton1Click:Connect(function()
-        (setclipboard or toclipboard)("https://dsc.gg/ln-hub")
+        (setclipboard or toclipboard)("https://discord.gg/25ms")
         wait(.1)
         game:GetService("StarterGui"):SetCore("SendNotification",{
             Title = "Discord",
